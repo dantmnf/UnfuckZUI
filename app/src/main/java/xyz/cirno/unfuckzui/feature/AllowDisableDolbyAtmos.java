6c3347d5809970e801076960c161b56e1d519059
dantmnf <dantmnf@users.noreply.github.com>
2024-12-18 21:29:08 +0800

    don't allow game service to toggle dolby

--- app/src/main/java/xyz/cirno/unfuckzui/feature/AllowDisableDolbyAtmos.java
+++ app/src/main/java/xyz/cirno/unfuckzui/feature/AllowDisableDolbyAtmos.java
@@ -9,7 +9,7 @@ import xyz.cirno.unfuckzui.FeatureRegistry;
 
 public class AllowDisableDolbyAtmos {
     public static final String FEATURE_NAME = "allow_disable_dax";
-    public static final FeatureRegistry.Feature FEATURE = new FeatureRegistry.Feature(FEATURE_NAME, new String[] {"com.android.settings", "com.android.systemui"}, AllowDisableDolbyAtmos::handleLoadPackage);
+    public static final FeatureRegistry.Feature FEATURE = new FeatureRegistry.Feature(FEATURE_NAME, new String[] {"com.android.settings", "com.android.systemui", "com.zui.game.service"}, AllowDisableDolbyAtmos::handleLoadPackage);
     public static void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
         if ("com.android.settings".equals(lpparam.packageName)) {
             if (Build.VERSION.SDK_INT == Build.VERSION_CODES.TIRAMISU) {
@@ -20,6 +20,8 @@ public class AllowDisableDolbyAtmos {
         } else if ("com.android.systemui".equals(lpparam.packageName)) {
             XposedHelpers.findAndHookMethod("com.android.systemui.qs.tiles.QDolbyAtmosTile", lpparam.classLoader, "isHeadSetConnect", XC_MethodReplacement.returnConstant(Boolean.TRUE));
             XposedHelpers.findAndHookMethod("com.android.systemui.qs.tiles.QDolbyAtmosDetailView", lpparam.classLoader, "isHeadSetConnect", XC_MethodReplacement.returnConstant(Boolean.TRUE));
+        } else if ("com.zui.game.service".equals(lpparam.packageName)) {
+            XposedHelpers.findAndHookMethod("com.zui.game.service.util.DolbyUtils", lpparam.classLoader, "handleDolbyGameSound", android.content.Context.class, int.class, XC_MethodReplacement.returnConstant(null));
         }
     }
 }
