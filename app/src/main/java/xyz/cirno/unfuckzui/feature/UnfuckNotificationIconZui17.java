4fbaadf2b6d926f3281e2db45b42f6e13b7408ca
dantmnf <2252500+dantmnf@users.noreply.github.com>
2025-11-02 15:48:04 +0800

    fix more notification icons
    e.g. LSPosed notification

--- app/src/main/java/xyz/cirno/unfuckzui/feature/UnfuckNotificationIconZui17.java
+++ app/src/main/java/xyz/cirno/unfuckzui/feature/UnfuckNotificationIconZui17.java
@@ -1,7 +1,5 @@
 package xyz.cirno.unfuckzui.feature;
 
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 import android.os.Build;
 import android.service.notification.StatusBarNotification;
 import android.util.TypedValue;
@@ -40,6 +38,7 @@ public class UnfuckNotificationIconZui17 {
             }
         });
 
+        // use grayscale icons for notification shelf (collapsed notification icons)
         XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.NotificationShelf", lpparam.classLoader, "updateResources$5", new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
@@ -51,13 +50,13 @@ public class UnfuckNotificationIconZui17 {
             }
         });
 
+        // don't replace the small icon with app icon
         XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.NotificationListener", lpparam.classLoader, "replaceTheSmallIcon", StatusBarNotification.class, XC_MethodReplacement.returnConstant(null));
 
         final var notificationHeaderViewWrapper_class = XposedHelpers.findClass("com.android.systemui.statusbar.notification.row.wrapper.NotificationHeaderViewWrapper", lpparam.classLoader);
         final var notificationHeaderViewWrapper_mIcon = XposedHelpers.findField(notificationHeaderViewWrapper_class, "mIcon");
         final var getIcon = MethodHandles.lookup().unreflectGetter(notificationHeaderViewWrapper_mIcon);
         final var expandableNotificationRow_class = XposedHelpers.findClass("com.android.systemui.statusbar.notification.row.ExpandableNotificationRow", lpparam.classLoader);
-        final var notificationRowIconView_class = Class.forName("com.android.internal.widget.NotificationRowIconView");
         XposedHelpers.findAndHookMethod(notificationHeaderViewWrapper_class, "onContentUpdated", expandableNotificationRow_class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -73,30 +72,16 @@ public class UnfuckNotificationIconZui17 {
                     if (lp instanceof ViewGroup.MarginLayoutParams mlp) {
                         mlp.setMarginStart(Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 12, dm)));
                     }
-                    if (iconview.getClass() != notificationRowIconView_class) {
-                        // notifications with no circle template
-                        // apply android:padding="@dimen/notification_icon_circle_padding"
-                        var padding = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, dm));
-                        iconview.setPadding(padding, padding, padding, padding);
-                        // apply android:background="@drawable/notification_icon_circle"
-                        var d = new ShapeDrawable();
-                        d.setShape(new OvalShape());
-                        d.getPaint().setColor(0xFF333333);
-                        iconview.setBackground(d);
-                    }
                     iconview.requestLayout();
                     iconview.setTag(KEY_SIZE_UNFUCKED, Boolean.TRUE);
                 }
             }
         });
 
-        XposedHelpers.findAndHookConstructor("com.android.systemui.statusbar.phone.NotificationIconContainer", lpparam.classLoader, "android.content.Context", "android.util.AttributeSet", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                XposedHelpers.setBooleanField(param.thisObject, "mOverrideIconColor", false);
-            }
-        });
-
+        // always true for ROW
         XposedHelpers.findAndHookMethod("com.android.systemui.notificationlist.view.NotificationHeaderView", lpparam.classLoader, "shouldShowIconBackground", XC_MethodReplacement.returnConstant(true));
+
+        // always use circle template for android.app.Notification$Builder#get*Resource()
+        XposedHelpers.findAndHookMethod("android.app.Notification$Builder", lpparam.classLoader, "isCtsGtsTest", XC_MethodReplacement.returnConstant(true));
     }
 }
