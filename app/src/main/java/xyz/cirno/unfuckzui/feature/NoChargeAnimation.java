66c3ce369c2816209f4a1d41f7df8abc32d9b2c2
dantmnf <2252500+dantmnf@users.noreply.github.com>
2025-11-03 23:03:44 +0800

    add missing file

---
+++ app/src/main/java/xyz/cirno/unfuckzui/feature/NoChargeAnimation.java
@@ -0,0 +1,32 @@
+package xyz.cirno.unfuckzui.feature;
+
+import android.os.Build;
+import android.os.Message;
+import android.service.notification.StatusBarNotification;
+import android.util.TypedValue;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Objects;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import xyz.cirno.unfuckzui.FeatureRegistry;
+
+public class NoChargeAnimation {
+    public static final String FEATURE_NAME = "no_charge_animation";
+    public static final FeatureRegistry.Feature FEATURE = new FeatureRegistry.Feature(FEATURE_NAME, new String[] {"com.android.systemui"}, NoChargeAnimation::handleLoadPackage);
+    public static void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        new NoChargeAnimation().handleLoadSystemUi(lpparam);
+    }
+
+    public void handleLoadSystemUi(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        var classLoader = lpparam.classLoader;
+        var ChargingAnimationControllerClass = classLoader.loadClass("com.android.keyguard.lockscreen.charge.ChargingAnimationController");
+        var handlerField = XposedHelpers.findField(ChargingAnimationControllerClass, "H");
+        XposedHelpers.findAndHookMethod(handlerField.getType(), "handleMessage", Message.class, XC_MethodReplacement.DO_NOTHING);
+    }
+}
