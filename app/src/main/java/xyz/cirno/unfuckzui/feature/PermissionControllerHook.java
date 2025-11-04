14c4804cebea8373a4a3da22d3070a69ee56cf2d
dantmnf <2252500+dantmnf@users.noreply.github.com>
2025-11-05 00:56:09 +0800

    no more zui permission controller

--- app/src/main/java/xyz/cirno/unfuckzui/feature/PermissionControllerHook.java
+++ app/src/main/java/xyz/cirno/unfuckzui/feature/PermissionControllerHook.java
@@ -1,8 +1,12 @@
 package xyz.cirno.unfuckzui.feature;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.view.Window;
+
+import java.lang.invoke.MethodHandles;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -13,8 +17,73 @@ import xyz.cirno.unfuckzui.FeatureRegistry;
 
 public class PermissionControllerHook {
     public static final String FEATURE_NAME = "permission_controller_style";
-    public static final FeatureRegistry.Feature FEATURE = new FeatureRegistry.Feature(FEATURE_NAME, new String[] {"com.android.permissioncontroller"}, PermissionControllerHook::handleLoadPackage);
-    public static void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+    public static final FeatureRegistry.Feature FEATURE = new FeatureRegistry.Feature(FEATURE_NAME, new String[] {"com.android.permissioncontroller", "com.android.settings", "com.zui.safecenter"}, PermissionControllerHook::handleLoadPackage);
+    public static void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if ("com.android.permissioncontroller".equals(lpparam.packageName)) {
+            handleLoadPermissionController(lpparam);
+        } else if ("com.android.settings".equals(lpparam.packageName)) {
+            new SettingsHook().handleLoadSettings(lpparam);
+        } else if ("com.zui.safecenter".equals(lpparam.packageName)) {
+            handleLoadSafeCenter(lpparam);
+        }
+    }
+
+    private static void handleLoadSafeCenter(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        var cls = XposedHelpers.findClass("com.lenovo.xuipermissionmanager.XuiPermissionManager", lpparam.classLoader);
+        var supercls = cls.getSuperclass();
+        var onCreate = XposedHelpers.findMethodExact(cls, "onCreate", Bundle.class);
+        var super_onCreate = XposedHelpers.findMethodExact(supercls, "onCreate", Bundle.class);
+        final var super_onCreate_invokespecial = MethodHandles.lookup().unreflectSpecial(super_onCreate, cls);
+        XposedBridge.hookMethod(onCreate, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                // redirect to AOSP permission manager
+                super_onCreate_invokespecial.invoke(param.thisObject, param.args[0]);
+                var activity = (Activity)param.thisObject;
+                activity.startActivity(new Intent("android.intent.action.MANAGE_PERMISSIONS"));
+                activity.finish();
+                param.setResult(null);
+            }
+        });
+        XposedHelpers.findAndHookMethod(cls, "onDestroy", XC_MethodReplacement.DO_NOTHING);
+    }
+
+    static class SettingsHook {
+        private final ThreadLocal<Boolean> isRowVersionTls = new ThreadLocal<>();
+
+        private class IsRowVersionHook extends XC_MethodHook {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                var value = isRowVersionTls.get();
+                if (value != null) {
+                    param.setResult(value);
+                }
+            }
+        }
+
+        private class IsRowVersionTlsHook extends XC_MethodHook {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                isRowVersionTls.set(true);
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                isRowVersionTls.remove();
+            }
+        }
+
+        public void handleLoadSettings(XC_LoadPackage.LoadPackageParam lpparam) {
+            XposedHelpers.findAndHookMethod("com.lenovo.common.utils.LenovoUtils", lpparam.classLoader, "isRowVersion", new IsRowVersionHook());
+
+            // make settings invoke AOSP permission manager
+            XposedHelpers.findAndHookMethod("com.android.settings.applications.appinfo.AppPermissionPreferenceController", lpparam.classLoader, "startManagePermissionsActivity", new IsRowVersionTlsHook());
+            XposedHelpers.findAndHookMethod("com.lenovo.settings.privacy.PrivacyManagerPreferenceController", lpparam.classLoader, "handlePreferenceTreeClick", "androidx.preference.Preference", new IsRowVersionTlsHook());
+            XposedHelpers.findAndHookMethod("com.lenovo.settings.applications.LenovoAppHeaderPreferenceController", lpparam.classLoader, "lambda$initAppEntryList$0$com-lenovo-settings-applications-LenovoAppHeaderPreferenceController", "android.view.View", new IsRowVersionTlsHook());
+        }
+    }
+
+    public static void handleLoadPermissionController(XC_LoadPackage.LoadPackageParam lpparam) {
         Class<?> zuiUtilsCls = XposedHelpers.findClassIfExists("com.android.permissioncontroller.extra.ZuiUtils", lpparam.classLoader);
         if (zuiUtilsCls == null) {
             zuiUtilsCls = XposedHelpers.findClassIfExists("com.android.permissioncontroller.permission.utils.ZuiUtils", lpparam.classLoader);
